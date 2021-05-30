079118daec35e99c73408a723c07113b80fd32c4
UgurCimenoglu <ugurcimenogluu@gmail.com>
2021-05-31 00:48:51 +0300

    Katmanlar oluşturuldu ve job titles için api yazıldı.

---
+++ HRMS/src/main/java/kodlamaio/HRMS/api/JobTitlesController.java
@@ -0,0 +1,30 @@
+package kodlamaio.HRMS.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.HRMS.business.abstracts.JobTitleService;
+import kodlamaio.HRMS.entities.concretes.JobTitle;
+
+@RestController
+@RequestMapping("/api/jobtitles")
+public class JobTitlesController {
+
+	private JobTitleService jobTitleService;
+
+	@Autowired
+	public JobTitlesController(JobTitleService jobTitleService) {
+		super();
+		this.jobTitleService = jobTitleService;
+	}
+
+	@GetMapping("/getall")
+	public List<JobTitle> getAll() {
+		return jobTitleService.getAll();
+	};
+
+}
---
+++ HRMS/src/main/java/kodlamaio/HRMS/dataAccess/abstracts/JobTitleDao.java
@@ -0,0 +1,9 @@
+package kodlamaio.HRMS.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kodlamaio.HRMS.entities.concretes.JobTitle;
+
+public interface JobTitleDao extends JpaRepository<JobTitle, Integer>{
+
+}
